388c795e9749b6225331bcf4be370fba8849a780
lostcache <harshdevisha9111@gmail.com>
2026-03-23 18:44:00 -0700

    fix treesitter config for newer version

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
 		config = function()
 			local ts = require("nvim-treesitter")
 
-			-- Install parsers
 			ts.install({
 				"go",
 				"cpp",
@@ -18,8 +17,15 @@ return {
 				"lua",
 				"markdown",
 				"markdown_inline",
+				"svelte",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "go", "c", "cpp", "python", "rust", "zig", "ocaml", "lua", "markdown", "svelte" },
+				callback = function()
+					vim.treesitter.start()
+				end,
 			})
 		end,
 	},
 }
-
