b8a396fb500c665e59aa08a2dc008574ed01ac0e
lostcache <harshdevisha9111@gmail.com>
2026-04-05 15:29:59 -0700

    fixes treesitter issues

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,35 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
 		build = ":TSUpdate",
 		lazy = false,
 		config = function()
-			local ts = require("nvim-treesitter")
+			-- v1 API (active until :Lazy update nvim-treesitter switches to master branch)
+			local ok, ts = pcall(require, "nvim-treesitter")
+			if ok and ts.install then
+				ts.install({
+					"go", "cpp", "c", "python", "rust", "zig",
+					"ocaml", "lua", "markdown", "markdown_inline", "svelte",
+				})
+			end
 
-			ts.install({
-				"go",
-				"cpp",
-				"c",
-				"python",
-				"rust",
-				"zig",
-				"ocaml",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-			})
+			-- master branch API (active after :Lazy update nvim-treesitter)
+			local ok2, configs = pcall(require, "nvim-treesitter.configs")
+			if ok2 then
+				configs.setup({
+					ensure_installed = {
+						"go", "cpp", "c", "python", "rust", "zig",
+						"ocaml", "lua", "markdown", "markdown_inline", "svelte",
+					},
+					highlight = { enable = true },
+				})
+			end
 
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = { "go", "c", "cpp", "python", "rust", "zig", "ocaml", "lua", "markdown", "svelte" },
 				callback = function()
-					vim.treesitter.start()
+					pcall(vim.treesitter.start)
 				end,
 			})
 		end,
