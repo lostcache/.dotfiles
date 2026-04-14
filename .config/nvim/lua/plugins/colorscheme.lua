bc43c6a6f89aaef44609f833091f00124a31cae6
lostcache <harshdevisha9111@gmail.com>
2026-04-14 04:36:36 -0700

    add new weird gruvbox theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -26,4 +26,59 @@ return {
 		lazy = false,
 		priority = 1000,
 	},
+
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true,
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = true,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true,
+				contrast = "hard",
+				palette_overrides = {},
+				overrides = {
+					["String"] = { fg = "#99FF20" },
+					["@string"] = { fg = "#99FF20" },
+
+					["Type"] = { fg = "#9090FF" },
+					["Function"] = { fg = "#EED030" },
+
+					["Identifier"] = { fg = "#75EEFF" },
+
+					["@variable"] = { fg = "#00AAD0" },
+					["@variable.parameter"] = {
+						italic = true,
+						fg = "#b9d3eb",
+					},
+					["@lsp.type.parameter"] = {
+						italic = true,
+						fg = "#b9d3eb",
+					},
+
+					["@constant"] = { fg = "#00AAD0" },
+
+					["Boolean"] = { fg = "#FF20FF" },
+					["StorageClass"] = { fg = "#E950A0" },
+				},
+				dim_inactive = false,
+				transparent_mode = true,
+			})
+		end,
+	},
 }
